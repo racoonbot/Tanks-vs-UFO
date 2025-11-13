8f4585328393864b3aa87267a9e774c7b11f4c8f
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-13 15:55:40 +0300

    Revert "h  враг движется к игроку"
    This reverts commit 091bd4b5b73eabfb1f9e895192d27c0a11ab5e0f.

--- Assets/Enemy/EnemyMoving.cs
+++ Assets/Enemy/EnemyMoving.cs
@@ -4,26 +4,15 @@ using UnityEngine;
 
 public class EnemyMoving : MonoBehaviour
 {
-    public float speed;
-    public GameObject target;
-    private Vector3 targetPos;
-
     // Start is called before the first frame update
     void Start()
     {
-        target = FindObjectOfType<Tank>().gameObject;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdateDirection();
-    }
-
-    public void UpdateDirection()
-    {
-        targetPos = target.transform.position;
-        Vector3 direction = (target.transform.position - transform.position).normalized;
-        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        
     }
 }
