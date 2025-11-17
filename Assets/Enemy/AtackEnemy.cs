4c99e375823c629f0ee49c4c2eb048cafe67332a
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-17 12:30:04 +0300

    f Три типа врагов

--- Assets/Enemy/AtackEnemy.cs
+++ Assets/Enemy/AtackEnemy.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AtackEnemy : MonoBehaviour
+public class AtackEnemy : EnemyBase
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
