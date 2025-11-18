ec15525b6ee2a997602530f87459a99297575160
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-18 10:45:49 +0300

    h сделан лун заморозки врагов

---
+++ Assets/Loot/BustTime.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BustTime : Loot
+{
+    public override IEnumerator StartBust(Tank tank)
+    {
+        if (tank == null) yield break;
+
+        RandomSpawner spawner = FindObjectOfType<RandomSpawner>();
+        if (spawner != null)
+        {
+            foreach (GameObject enemyObj in spawner.Enemies)
+            {
+                EnemyBase enemy = enemyObj.GetComponent<EnemyBase>();
+                if (enemy != null) enemy.speed = 0f;
+            }
+        }
+        yield return new WaitForSeconds(boostDuration);
+        if (spawner != null)
+        {
+            foreach (GameObject enemyObj in spawner.Enemies)
+            {
+                EnemyBase enemy = enemyObj.GetComponent<EnemyBase>();
+                if (enemy != null) enemy.speed = 3f;
+            }
+        }
+    }
+}
