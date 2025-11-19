55b5151b35df6ff4c2301a18c9bf19967cea1014
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-11-19 11:48:09 +0300

    f спавнер лута. Начало

--- Assets/LootSpawner.cs
+++ Assets/LootSpawner.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LootSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float MaxSpawnPointX = 28f;
+    private float MaxSpawnPointZ = 28f;
+    private float MinSpawnPointX = -28f;
+    private float MinSpawnPointZ = -28f;
+
+    public int LootAmount = 10;
+
+    public List<GameObject> loot = new List<GameObject>();
+
+    private void Start()
     {
-        
+        LootSpawn();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LootSpawn()
     {
-        
+        for (int i = 0; i < LootAmount; i++)
+        {
+            GameObject RandomLootItem = loot[Random.Range(0, loot.Count)];
+            Vector3 spawnPosition = RandomSpawnPoint();
+            Instantiate(RandomLootItem, spawnPosition, Quaternion.identity);
+        }
+    }
+
+    private Vector3 RandomSpawnPoint()
+    {
+        return new Vector3(Random.Range(MinSpawnPointX, MaxSpawnPointX), 0f,
+            Random.Range(MinSpawnPointZ, MaxSpawnPointZ));
     }
 }
