633edaaa1c1a8eab73c405d943a0a7d9d9bd7e99
racoonbot <zozipaa@gmail.com>
2025-12-03 16:54:27 +0300

    f добавлены фоны. Переделан стартовй ui

--- Assets/Loot/Loot.cs
+++ Assets/Loot/Loot.cs
@@ -7,6 +7,14 @@ public abstract class Loot : MonoBehaviour
 {
     public float boostDuration;
     public bool isBoosted = false;
+    private Sounds sounds;
+
+
+    private void Start()
+    {
+        sounds = FindObjectOfType<Sounds>();
+        if(sounds ==  null) Debug.LogError("No Sounds in loot Found");
+    }
 
     protected virtual void ActivateBust()
     {
@@ -26,6 +34,7 @@ public abstract class Loot : MonoBehaviour
         Debug.Log("OnTriggerEnter");
         if (other.GetComponent<TankHealth>())
         {
+            sounds.source[0].Play();
             ActivateBust();
             gameObject.SetActive(false);
             Destroy(gameObject, 10f);
