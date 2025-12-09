1118feda47aa2d105df42aae5cab97ea0429071f
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-12-09 12:46:07 +0300

    h UI upgrade

--- Assets/TankAttributes.cs
+++ Assets/TankAttributes.cs
@@ -1,15 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class TankAttributes : MonoBehaviour
 {
     public float maxSpeed;
-    public float maxHealth;
-    public int damage;
-    public float turretRotationSpeed;
+    public float maxHealth; // Лучше использовать float, раз уж начали, или приводить типы
+    public float turretRotationSpeed; // Добавил, раз мы его улучшаем
 
+    private void Start()
+    {
+        LoadStats();
+    }
 
     public void ApplyUpgrade(UpgradeType type, float amount)
     {
@@ -17,13 +17,27 @@ public class TankAttributes : MonoBehaviour
         {
             case UpgradeType.MaxHealth:
                 maxHealth += amount;
+                PlayerPrefs.SetFloat("Value_MaxHealth", maxHealth); 
                 break;
+
             case UpgradeType.TowerRotation:
                 turretRotationSpeed += amount;
+                PlayerPrefs.SetFloat("Value_TowerRotation", turretRotationSpeed);
                 break;
+
             case UpgradeType.MaxSpeed:
                 maxSpeed += amount;
+                PlayerPrefs.SetFloat("Value_MaxSpeed", maxSpeed);
                 break;
         }
+        
+        PlayerPrefs.Save();
+    }
+
+    private void LoadStats()
+    {
+        maxHealth = PlayerPrefs.GetFloat("Value_MaxHealth", maxHealth);
+        maxSpeed = PlayerPrefs.GetFloat("Value_MaxSpeed", maxSpeed);
+        turretRotationSpeed = PlayerPrefs.GetFloat("Value_TowerRotation", turretRotationSpeed);
     }
 }
