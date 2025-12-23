410fcb9de262aa22e1c09daa3414174899e37a7a
racoonbot <zozipaa@gmail.com>
2025-12-23 19:15:32 +0300

    f Отображение максимальной скорости
    + начало работы на UI  Настроек на главном экране

--- Assets/ShowMaxSpeed.cs
+++ Assets/ShowMaxSpeed.cs
@@ -6,10 +6,24 @@ using UnityEngine;
 public class ShowMaxSpeed : MonoBehaviour
 {
     public TankAttributes tankAttributes;
+    public Rigidbody tankRigidbody; // Нужно перетащить Rigidbody танка в инспекторе
     public TextMeshProUGUI text;
 
     void Update()
     {
-        text.text = tankAttributes.maxSpeed.ToString();
+        // Рассчитываем физический предел скорости
+        // Формула: (Сила * FixedDeltaTime) / (Масса * Сопротивление)
+        float drag = tankRigidbody.drag;
+        float mass = tankRigidbody.mass;
+        
+        if (drag > 0) 
+        {
+            float calculatedMaxSpeed = ((tankAttributes.maxSpeed * Time.fixedDeltaTime) / (mass * drag)) - 0.1f;
+            text.text = calculatedMaxSpeed.ToString("F2");
+        }
+        else 
+        {
+            text.text = "Infinity"; // Без Drag скорость не ограничена
+        }
     }
 }
