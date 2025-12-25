1bf2038014dc12650bcc61a82fa9a9117295af9d
racoonbot <41879013+racoonbot@users.noreply.github.com>
2025-12-25 15:35:03 +0300

    h Добавлен звук лута для уничтожения пуль

--- Assets/Bullet/BulletDestroyer.cs
+++ Assets/Bullet/BulletDestroyer.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 public class BulletDestroyer : MonoBehaviour
 {
     public static Action OnGlobalDestruction;
+   
 
     private void OnEnable()
     {
         OnGlobalDestruction += DestroySelf;
+
     }
 
     private void OnDisable()
     {
         OnGlobalDestruction -= DestroySelf;
+
     }
 
     private void DestroySelf()
     {
-
+        
         Destroy(gameObject); 
     }
+
+   
+    
 }
--- Assets/BustBulletDestr.cs
+++ Assets/BustBulletDestr.cs
@@ -2,13 +2,24 @@ using System;
 using UnityEngine;
 
 public class BustBulletDestr : Loot
+
 {
+    private Sounds sounds;
+
+
+    private void Start()
+    {
+        sounds = FindObjectOfType<Sounds>();
+        if(sounds ==  null) Debug.LogError("No Sounds in loot Found");
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.name);
         if (other.GetComponent<TankHealth>())
         {
             BulletDestroyer.OnGlobalDestruction?.Invoke();
+            sounds.source[0].Play();
             Destroy(gameObject);
         }
     }
