186fe8bd6957f658b83c099492e79b240618da37
racoonbot <zozipaa@gmail.com>
2025-12-27 13:24:23 +0300

    f upd

--- Assets/ShowMaxSpeed.cs
+++ Assets/ShowMaxSpeed.cs
@@ -6,24 +6,22 @@ using UnityEngine;
 public class ShowMaxSpeed : MonoBehaviour
 {
     public TankAttributes tankAttributes;
-    public Rigidbody tankRigidbody; // Нужно перетащить Rigidbody танка в инспекторе
+    public Rigidbody tankRigidbody;
     public TextMeshProUGUI text;
 
     void Update()
     {
-        // Рассчитываем физический предел скорости
-        // Формула: (Сила * FixedDeltaTime) / (Масса * Сопротивление)
         float drag = tankRigidbody.drag;
         float mass = tankRigidbody.mass;
         
         if (drag > 0) 
         {
             float calculatedMaxSpeed = ((tankAttributes.maxSpeed * Time.fixedDeltaTime) / (mass * drag)) - 0.1f;
-            text.text = calculatedMaxSpeed.ToString("F2");
+            text.text = calculatedMaxSpeed.ToString("F1");
         }
         else 
         {
-            text.text = "Infinity"; // Без Drag скорость не ограничена
+            text.text = "Infinity"; 
         }
     }
 }
