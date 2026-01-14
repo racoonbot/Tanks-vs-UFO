b156dfed7b2378e99e7674438e4a4706188a6788
racoonbot <41879013+racoonbot@users.noreply.github.com>
2026-01-14 13:36:15 +0300

    тест лидерборда. Билд

--- Assets/LeaderBoard.cs
+++ Assets/LeaderBoard.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
-
+using YG; 
 public class LeaderBoard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private TankHealth tankHealth;
+   private Wallet wallet;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void Start()
+   {
+       tankHealth = FindObjectOfType<TankHealth>();
+       wallet = FindObjectOfType<Wallet>();
+       if (wallet == null)
+       {
+           Debug.Log("Wallet not found");
+       }
+       if (tankHealth != null)
+       {
+           tankHealth.OnDeathPlayer += SaveMyScore;
+       }
+   }
+
+   private void OnDisable()
+   {
+       tankHealth.OnDeathPlayer -= SaveMyScore;
+   }
+   public void SaveMyScore()
+   {
+       Debug.Log("SaveMyScore");
+       int finalScore = wallet.totalMoney; 
+       YG2.SetLeaderboard("testlb", finalScore);
+   }
 }
+
