1732dfa75e2e00e22126d495e1f21c095d577c7b
racoonbot <41879013+racoonbot@users.noreply.github.com>
2026-01-16 15:02:48 +0300

    h исправлен метод записи в лидерборд

--- Assets/LeaderBoard.cs
+++ Assets/LeaderBoard.cs
@@ -1,37 +1,66 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using YG; 
+using YG;
+using YG.Utils.LB; // Нужно для работы с LBData и LBPlayerData
+
 public class LeaderBoard : MonoBehaviour
 {
-   private TankHealth tankHealth;
-   private Wallet wallet;
+    private TankHealth tankHealth;
+    private Wallet wallet;
 
-   private void Start()
-   {
-       tankHealth = FindObjectOfType<TankHealth>();
-       wallet = FindObjectOfType<Wallet>();
-       if (wallet == null)
-       {
-           Debug.Log("Wallet not found");
-       }
-       if (tankHealth != null)
-       {
-           tankHealth.OnDeathPlayer += SaveMyScore;
-       }
-   }
+    private int pendingScoreToSave;
+    private bool isCheckingScore;
 
-   private void OnDisable()
-   {
-       tankHealth.OnDeathPlayer -= SaveMyScore;
-   }
-   public void SaveMyScore()
-   {
-       Debug.Log("SaveMyScore");
-       int finalScore = wallet.totalMoney; 
-       YG2.SetLeaderboard("testlb", finalScore);
-   }
+    private const string LeaderboardName = "testlb";
+
+    private void Start()
+    {
+        tankHealth = FindObjectOfType<TankHealth>();
+        wallet = FindObjectOfType<Wallet>();
+        
+        YG2.onGetLeaderboard += OnLeaderboardDataReceived;
+        if (tankHealth != null)
+        {
+            tankHealth.OnDeathPlayer += SaveMyScore;
+        }
+    }
+
+    private void OnDisable()
+    {
+        YG2.onGetLeaderboard -= OnLeaderboardDataReceived;
+        
+        if (tankHealth != null)
+        {
+            tankHealth.OnDeathPlayer -= SaveMyScore;
+        }
+    }
+    private void SaveMyScore()
+    {
+        pendingScoreToSave = wallet.totalMoney;
+        isCheckingScore = true;
+        YG2.GetLeaderboard(LeaderboardName);
+    }
+
+    private void OnLeaderboardDataReceived(LBData data)
+    {
+        if (!isCheckingScore || data.technoName != LeaderboardName) return;
+
+        isCheckingScore = false; 
+        int oldRecord = 0;
+        if (data.players != null)
+        {
+            foreach (var entry in data.players)
+            {
+                if (entry.name == YG2.player.id)
+                {
+                    oldRecord = entry.score; 
+                    break; 
+                }
+            }
+        }
+
+        if (pendingScoreToSave > oldRecord)
+        {
+            YG2.SetLeaderboard(LeaderboardName, pendingScoreToSave);
+        }
+    }
 }
-
