19bd464a88aaccd9ae5099e21f9671612bcb03bd
racoonbot <zozipaa@gmail.com>
2026-01-17 14:31:10 +0300

    f  добавлены англ кнопки и моджуль локализации

---
+++ Assets/UI/LocalizeButtons.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using YG; 
+
+public class LocalizeButtons : MonoBehaviour
+{
+    [Header("Картинки для языков")]
+    public Sprite ruSprite;
+    public Sprite enSprite;
+
+    private Image imageComponent;
+
+    private void Awake()
+    {
+        imageComponent = GetComponent<Image>();
+    }
+
+    private void OnEnable()
+    {
+        // В YG2 событие коррекции языка называется именно так
+        YG2.onCorrectLang += ApplyLanguage;
+
+        // ИСПРАВЛЕНО: SDKEnabled заменен на isSDKEnabled
+        if (YG2.isSDKEnabled)
+        {
+            ApplyLanguage(YG2.lang);
+        }
+    }
+
+    private void OnDisable()
+    {
+        YG2.onCorrectLang -= ApplyLanguage;
+    }
+
+    private void ApplyLanguage(string lang)
+    {
+        if (lang == "ru")
+        {
+            if (ruSprite != null) imageComponent.sprite = ruSprite;
+        }
+        else
+        {
+            if (enSprite != null) imageComponent.sprite = enSprite;
+        }
+    }
+}
