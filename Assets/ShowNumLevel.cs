8f6d38d404342737a1ab291956b2c2b8b6b3902c
racoonbot <zozipaa@gmail.com>
2026-04-03 12:42:58 +0300

    перевод магазина

--- Assets/ShowNumLevel.cs
+++ Assets/ShowNumLevel.cs
@@ -1,6 +1,6 @@
 using TMPro;
 using UnityEngine;
-using YG;
+
 
 public class ShowNumLevel : MonoBehaviour
 {
@@ -17,17 +17,6 @@ public class ShowNumLevel : MonoBehaviour
 
     public void UpdateText()
     {
-        if (YG2.lang == "en")
-        {
-            text.text = $"Level  {levelManager.level}";
-        }
-        else if (YG2.lang == "ru")
-        {
-            text.text = $"Уровень  {levelManager.level}";
-        }
-        else
-        {
-            text.text = $"Level  {levelManager.level}";
-        }
+        text.text = $"Level  {levelManager.level}";
     }
 }
